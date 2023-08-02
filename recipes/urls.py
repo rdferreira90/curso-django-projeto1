e99980934dea70a3e7073c1df0c354b0ef3750db
Rodrigo da Silva Ferreira <rdferreira90@gmail.com>
2023-08-02 08:12:57 -0300

    Removido Contato e Sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 
 urlpatterns = [
-    path("", home),  # HOME
-    path("sobre/", sobre),  # /sobre/
-    path("contato", contato),  # /contato/
+    path("", home),  # HOME    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={"name": "Rodrigo Ferreira"})
-
-
-def contato(request):
-    return render(request, "recipes/contato.html")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
+    return render(request, "recipes/home.html", context={
+        "name": "Rodrigo Ferreira",
+    })
